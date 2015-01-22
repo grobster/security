9292eb6d7d3d23db874a4ca8fff10efb26504814
grobster <jquarles@grobster.com>
2015-01-22 17:36:12 -0500

    password protected the app

--- myprojects/source/com/grobster/security/TestDrive.java
+++ myprojects/source/com/grobster/security/TestDrive.java
@@ -9,26 +9,16 @@ public class TestDrive {
 		String password = "123456";
 		
 		try {
-			File file = new File("v.ser");
-			if (file.exists()) {
-				String otherPath = JOptionPane.showInputDialog(null, "Enter Password");
+			String otherPath = JOptionPane.showInputDialog(null, "Enter Password");
 				
-				if (otherPath.equals(password)) {
-					System.out.println("restored");
-					FileInputStream fileStream = new FileInputStream(file);
-					ObjectInputStream os = new ObjectInputStream(fileStream);
-					Object one = os.readObject();
-					FileViewer view = (FileViewer) one;
-					view.createView();
-					os.close();
-				}
-			} else {
+			if (otherPath.equals(password)) {
 				Path p = Paths.get("C:\\myprojects\\classes");
 				MyDirectoryInterface myDirectory = new MyDirectory(p);
 				FileViewer viewer = new FileViewer(myDirectory);
 				viewer.createView();
 				System.out.println("not restored. a new instance");
 			}
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
