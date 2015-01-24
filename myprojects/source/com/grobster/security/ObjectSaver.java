47a1cbae066b2e63953fa52770088c64b7270b12
grobster <jquarles@grobster.com>
2015-01-24 12:25:44 -0500

    java comments added to ObjectSaver interface

--- myprojects/source/com/grobster/security/ObjectSaver.java
+++ myprojects/source/com/grobster/security/ObjectSaver.java
@@ -2,6 +2,13 @@ package com.grobster.security;
 
 import java.io.*;
 
+/**
+ * This interface is used to save objects to a file.  Also,
+ * it rebuilds the object after it has been saved.
+ *
+ * @author Jeffery Quarles
+ * @since 1.0
+ */
 public interface ObjectSaver extends Serializable {
 	public void save(Object o);
 	public Object reconstruct(File file);
